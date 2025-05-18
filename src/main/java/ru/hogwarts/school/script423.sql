edd4de2ad72e016c6a6faec59e7bae21920aa6f9
monalisa <forrestlake@yandex.ru>
2025-05-18 17:28:53 +0300

    Сделано дз

--- src/main/java/ru/hogwarts/school/script423.sql
+++ src/main/java/ru/hogwarts/school/script423.sql
@@ -1,3 +1,3 @@
-SELECT student.name, student.age, faculty_name FROM student LEFT JOIN faculty ON student.faculty_id = faculty.id;
+SELECT student.name, student.age, faculty.name FROM student LEFT JOIN faculty ON student.faculty_id = faculty.id;
 
 SELECT student.name, student.age FROM student INNER JOIN avatar ON avatar_id = avatar.id;
